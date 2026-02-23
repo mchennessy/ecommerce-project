aec0a70372a49c629560981c4bb3f1e95ce7040e
Mark <mark@gripiron.com>
2026-02-22 19:59:45 -0500

    async await

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,12 @@ function App() {
     const [cart, setCart] = useState([]);
 
     useEffect(() => {
-        axios.get('/api/cart-items?expand=product')
-            .then((response) => {
-                setCart(response.data);
-            });
+        const getCartItems = async () => {
+            const response = await axios.get('/api/cart-items?expand=product');
+            setCart(response.data);
+        }
+
+        getCartItems();
     }, []);
 
   return (

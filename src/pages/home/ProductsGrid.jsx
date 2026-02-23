2ab2f5ef5c4cb4abd75e72e02a2802c148ef03ac
Mark <mark@gripiron.com>
2026-02-23 10:27:01 -0500

    added quantity support

--- src/pages/home/ProductsGrid.jsx
+++ src/pages/home/ProductsGrid.jsx
@@ -1,68 +1,20 @@
 import axios from 'axios';
-import { formatMoney } from '../../utils/money';
+import { ProductControl } from './ProductControl';
 
 export function ProductsGrid({ products, loadCart }) {
-    function addToCart(productId) {
-        axios.post('/api/cart-items', {
+    async function addToCart(productId, quantity) {
+        await axios.post('/api/cart-items', {
             productId: productId,
-            quantity: 1
-        }).then(() => { 
-            loadCart();
+            quantity: quantity
         });
+        await loadCart();
     }
 
 
     return (
         <div className="products-grid">
             {products.map((product) => (
-                <div key={product.id} className="product-container">
-                    <div className="product-image-container">
-                        <img className="product-image"
-                            src={product.image} />
-                    </div>
-
-                    <div className="product-name limit-text-to-2-lines">
-                        {product.name}
-                    </div>
-
-                    <div className="product-rating-container">
-                        <img className="product-rating-stars"
-                            src={`images/ratings/rating-${product.rating.stars * 10}.png`} />
-                        <div className="product-rating-count link-primary">
-                            {product.rating.count}
-                        </div>
-                    </div>
-
-                    <div className="product-price">
-                        {formatMoney(product.priceCents)}
-                    </div>
-
-                    <div className="product-quantity-container">
-                        <select>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
-                            <option value="7">7</option>
-                            <option value="8">8</option>
-                            <option value="9">9</option>
-                            <option value="10">10</option>
-                        </select>
-                    </div>
-
-                    <div className="product-spacer"></div>
-
-                    <div className="added-to-cart">
-                        <img src="images/icons/checkmark.png" />
-                        Added
-                    </div>
-
-                    <button className="add-to-cart-button button-primary" onClick={() => addToCart(product.id)}>
-                        Add to Cart
-                    </button>
-                </div>
+                <ProductControl key={product.id} product={product} addToCart={addToCart} />
             ))
             }
         </div>
